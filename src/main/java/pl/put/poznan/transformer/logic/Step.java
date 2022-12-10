aade5a50c0b84281171e9769422a313ecb611778
Grzegorz Płaczek <g01.placzek@gmail.com>
2022-12-10 21:00:58 +0100

    [#13] Add missing constructors, getters and setters to Step

--- src/main/java/pl/put/poznan/transformer/logic/Step.java
+++ src/main/java/pl/put/poznan/transformer/logic/Step.java
@@ -11,4 +11,15 @@ public class Step implements Serializable {
         this.text = text;
         this.steps = null;
     }
+    public Step(String text, ArrayList<Step> steps) {
+        this.text = text;
+        this.steps = steps;
+    }
+
+    public String getText() { return text; }
+
+    public void setText(String text) { this.text = text; }
+    public ArrayList<Step> getSteps() { return steps; }
+
+    public void addStep(Step step) { this.steps.add(step); }
 }
