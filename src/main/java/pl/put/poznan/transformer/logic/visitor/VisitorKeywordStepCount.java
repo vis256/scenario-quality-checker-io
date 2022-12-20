acc5eeeea78634fbf0c0010f649cc58aae5062ce
Helga151 <ogerlich151@gmail.com>
2022-12-20 21:01:54 +0100

    [#19] Counting Keywords

---
+++ src/main/java/pl/put/poznan/transformer/logic/visitor/VisitorKeywordStepCount.java
@@ -0,0 +1,30 @@
+package pl.put.poznan.transformer.logic.visitor;
+
+import pl.put.poznan.transformer.logic.Scenario;
+import pl.put.poznan.transformer.logic.Step;
+
+public class VisitorKeywordStepCount implements Visitor {
+    private int keywords = 0;
+
+    @Override
+    public void visit(Scenario scenario) {
+        for (Step substep : scenario.getSteps()) {
+            substep.accept( this );
+        }
+    }
+
+    @Override
+    public void visit(Step step) {
+        String text = step.getText();
+        if(text.startsWith("FOR EACH") || text.startsWith("IF") || text.startsWith("ELSE")) {
+            keywords++;
+        }
+        for (Step substep : step.getSteps()) {
+            substep.accept( this );
+        }
+    }
+
+    public int getKeywordsNumber() {
+        return keywords;
+    }
+}
--- src/main/java/pl/put/poznan/transformer/rest/KeywordStepCountController.java
+++ src/main/java/pl/put/poznan/transformer/rest/KeywordStepCountController.java
@@ -2,22 +2,23 @@ package pl.put.poznan.transformer.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pl.put.poznan.transformer.logic.QualityChecker;
 
 @RestController
 @RequestMapping("/keywordStepCount")
 public class KeywordStepCountController {
     private static final Logger log = LoggerFactory.getLogger(KeywordStepCountController.class);
 
+    private final QualityChecker qualityChecker;
+
+    public KeywordStepCountController(QualityChecker qc) { this.qualityChecker = qc; }
+
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public int get() {
         log.info("[GET] /keywordStepCount");
-
-        // TODO: Return actual data
-
-        return 13;
+        return this.qualityChecker.getKeywordsCount();
     }
 }
